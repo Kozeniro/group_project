e279024ba933c6abe1b31e1494828ad4da48f221
Nikolay <fake@email.com>
2026-01-09 17:01:56 +0300

    Созданы api энпоинты для действий в системе

--- main_module/src/main.cpp
+++ main_module/src/main.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 #include <pqxx/pqxx>
-
-#include "Api_resUsers.h"
-#include "Api_resCourse.h"
-#include "Api_resQuestions.h"
-#include "Api_resTests.h"
-#include "Api_resAnswers.h"
-#include "Api_resAttempt.h"
+#include "httplib.h"
+#include <register_routes.h>
 
 int main()
 {
     std::string connection_string = "postgres://bf39575214c9189fa70701554b75ce7dd71cc18accecae236c76748378e000a8:sk_keRvGx-d9MLa5h3yNF3Pk@db.prisma.io:5432/postgres?sslmode=require";
     pqxx::connection conn(connection_string);
 
-    pqxx::work worker(conn);
+    ResourceUsers resUsers(conn); ResourceCourse resCourse(conn); ResourceQuestions resQuestions(conn); 
+    ResourceTests resTests(conn); ResourceAnswers resAnswers(conn); ResourceAttempt resAttempt(conn, resAnswers);
 
-    pqxx::result response = worker.exec("SELECT * FROM users");
-    for (size_t i = 0; i < response.size(); i++)
-    {
-        std::cout << "id: " << response[i][0] << " full_name: " << response[i][1] << " is_blocked: " << response[i][2] << std::endl;
-    }
+    Api_resUsers api_users(resUsers); Api_resCourse api_course(resCourse); Api_resQuestions api_questions(resQuestions);
+    Api_resTests api_tests(resTests); Api_resAnswers api_answers(resAnswers); Api_resAttempt api_attempt(resAttempt);
+
+    httplib::Server svr;
+    register_routes(svr, api_users, api_course, api_questions, api_tests, api_answers, api_attempt);
     
+    svr.listen("0.0.0.0", 8080);
 }
---
+++ main_module/src/register_routes.cpp
@@ -0,0 +1,55 @@
+#include <register_routes.h>
+
+
+void register_routes(httplib::Server& svr, 
+	Api_resUsers& api_users, Api_resCourse& api_course, Api_resQuestions& api_questions, 
+	Api_resTests& api_tests, Api_resAnswers& api_answers, Api_resAttempt& api_attempt)
+{
+	//Resource USERS
+	svr.Get(R"(/api/users)", [&](const httplib::Request& req, httplib::Response& res) {api_users.get_all(req, res);});
+	svr.Get(R"(/api/users/(\d+)/name)", [&](const httplib::Request& req, httplib::Response& res) {api_users.get_name(req, res);});
+	svr.Put(R"(api/api/users/(\d+)/name)", [&](const httplib::Request& req, httplib::Response& res) {api_users.update_name(req, res);});
+	svr.Get(R"(/api/users/(\d+)/info)", [&](const httplib::Request& req, httplib::Response& res) {api_users.get_info(req, res);});
+	svr.Get(R"(/api/users/(\d+)/roles)", [&](const httplib::Request& req, httplib::Response& res) {api_users.get_roles(req, res);});
+	svr.Post(R"(/api/users/(\d+)/roles)", [&](const httplib::Request& req, httplib::Response& res) {api_users.set_roles(req, res);});
+	svr.Get(R"(/api/users/(\d+)/blocked)", [&](const httplib::Request& req, httplib::Response& res) {api_users.check_user_blocked(req, res);});
+	svr.Post(R"(/api/users/(\d+)/blocked)", [&](const httplib::Request& req, httplib::Response& res) {api_users.set_user_blocked(req, res);});
+	//Resource COURSE
+	svr.Get(R"(/api/course)", [&](const httplib::Request& req, httplib::Response& res) {api_course.get_all(req, res);});
+	svr.Get(R"(/api/course/(\d+)/info)", [&](const httplib::Request& req, httplib::Response& res) {api_course.get_info(req, res);});
+	svr.Put(R"(/api/course/(\d+)/info)", [&](const httplib::Request& req, httplib::Response& res) {api_course.update_info(req, res);});
+	svr.Get(R"(/api/course/(\d+)/tests)", [&](const httplib::Request& req, httplib::Response& res) {api_course.get_tests(req, res);});
+	svr.Get(R"(/api/course/(\d+)/tests/(\d+)/activity)", [&](const httplib::Request& req, httplib::Response& res) {api_course.is_test_active(req, res);});
+	svr.Post(R"(/api/course/(\d+)/tests/(\d+)/activity)", [&](const httplib::Request& req, httplib::Response& res) {api_course.set_test_active(req, res);});
+	svr.Post(R"(/api/course/(\d+)/tests)", [&](const httplib::Request& req, httplib::Response& res) {api_course.add_test(req, res);});
+	svr.Delete(R"(/api/course/(\d+)/tests/(\d+))", [&](const httplib::Request& req, httplib::Response& res) {api_course.remove_test(req, res);});
+	svr.Get(R"(/api/course/(\d+)/students)", [&](const httplib::Request& req, httplib::Response& res) {api_course.get_students(req, res);});
+	svr.Post(R"(/api/course/(\d+)/students)", [&](const httplib::Request& req, httplib::Response& res) {api_course.add_user(req, res);});
+	svr.Delete(R"(/api/course/(\d+)/students/<student_id>)", [&](const httplib::Request& req, httplib::Response& res) {api_course.remove_user(req, res);});
+	svr.Post(R"(/api/course)", [&](const httplib::Request& req, httplib::Response& res) {api_course.create_course(req, res);});
+	svr.Delete(R"(/api/course/(\d+))", [&](const httplib::Request& req, httplib::Response& res) {api_course.delete_course(req, res);});
+	//Resource QUESTIONS
+	svr.Get(R"(/api/questions)", [&](const httplib::Request& req, httplib::Response& res) {api_questions.get_all(req, res);});
+	svr.Get(R"(/api/questions/(\d+)?version=(int))", [&](const httplib::Request& req, httplib::Response& res) {api_questions.get_info(req, res);});
+	svr.Put(R"(/api/questions/(\d+))", [&](const httplib::Request& req, httplib::Response& res) {api_questions.update_question(req, res);});
+	svr.Post(R"(/api/questions)", [&](const httplib::Request& req, httplib::Response& res) {api_questions.create_question(req, res);});
+	svr.Delete(R"(/api/questions/(\d+))", [&](const httplib::Request& req, httplib::Response& res) {api_questions.delete_question(req, res);});
+	//Resource TESTS
+	svr.Delete(R"(/api/tests/(\d+)/questions/(\d+))", [&](const httplib::Request& req, httplib::Response& res) {api_tests.remove_question(req, res);});
+	svr.Post(R"(/api/tests/(\d+)/questions)", [&](const httplib::Request& req, httplib::Response& res) {api_tests.add_question(req, res);});
+	svr.Put(R"(/api/tests/(\d+)/questions)", [&](const httplib::Request& req, httplib::Response& res) {api_tests.set_question_order(req, res);});
+	svr.Get(R"(/api/tests/(\d+)/users)", [&](const httplib::Request& req, httplib::Response& res) {api_tests.get_users_completed(req, res);});
+	svr.Get(R"(/api/tests/(\d+)/scores)", [&](const httplib::Request& req, httplib::Response& res) {api_tests.get_users_scores(req, res);});
+	svr.Get(R"(/api/tests/(\d+)/answers)", [&](const httplib::Request& req, httplib::Response& res) {api_tests.get_users_answers(req, res);});
+	//Resource ATTEMPT
+	svr.Post(R"(/api/attempt)", [&](const httplib::Request& req, httplib::Response& res) {api_attempt.create_attempt(req, res);});
+	svr.Put(R"(/api/attempt/(\d+))", [&](const httplib::Request& req, httplib::Response& res) {api_attempt.update_answer(req, res);});
+	svr.Delete(R"(/api/attempt/(\d+))", [&](const httplib::Request& req, httplib::Response& res) {api_attempt.finish_attempt(req, res);});
+	svr.Get(R"(/api/attempt)", [&](const httplib::Request& req, httplib::Response& res) {api_attempt.get_info(req, res);});
+	//Resource ANSWERS
+	svr.Post(R"(/api/answers)", [&](const httplib::Request& req, httplib::Response& res) {api_answers.create_answer(req, res);});
+	svr.Get(R"(/api/answers/(\d+))", [&](const httplib::Request& req, httplib::Response& res) {api_answers.get_answer(req, res);});
+	svr.Put(R"(/api/answers/(\d+))", [&](const httplib::Request& req, httplib::Response& res) {api_answers.update_answer(req, res);});
+	svr.Delete(R"(/api/answers/(\d+))", [&](const httplib::Request& req, httplib::Response& res) {api_answers.delete_answer(req, res);});
+	
+}
