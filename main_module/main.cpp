baec119bf778ae2f0938b061da563e1bc8c8c625
Nikolay <fake@email.com>
2026-01-09 04:17:03 +0300

    Настройка docker и CMakeLists для библиотеки работы с базой данных

---
+++ main_module/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <pqxx/pqxx>
+
+int main()
+{
+    std::string connection_string = "postgres://bf39575214c9189fa70701554b75ce7dd71cc18accecae236c76748378e000a8:sk_keRvGx-d9MLa5h3yNF3Pk@db.prisma.io:5432/postgres?sslmode=require";
+    pqxx::connection conn(connection_string);
+
+    pqxx::work worker(conn);
+
+    pqxx::result response = worker.exec("SELECT * FROM users");
+    for (size_t i = 0; i < response.size(); i++)
+    {
+        std::cout << "id: " << response[i][0] << " full_name: " << response[i][1] << " is_blocked: " << response[i][2] << std::endl;
+    }
+    
+}
