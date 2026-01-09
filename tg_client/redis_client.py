121adba33e5d319350eda110664a05941f212b2e
Dani <gofanovdaniil@gmail.com>
2026-01-09 22:58:39 +0000

    Добавлены вспомагательные функции в redis_client.py для работы с токеном и сессиями

--- tg_client/redis_client.py
+++ tg_client/redis_client.py
@@ -1,5 +1,6 @@
 import redis    
 import json
+from datetime import datetime
 
 class RedisClient:
     def __init__(self):
@@ -32,5 +33,44 @@ class RedisClient:
         
         self.redis.delete(f"chat:{chat_id}")
 
+    def save_login_token(self, token, data):
+        if not self.redis:
+            return
+        
+        data_json = json.dumps(data)
+        self.redis.setex(f"token:{token}", 300, data_json)
+
+    def get_login_token(self, token):
+        if not self.redis:
+            return None
+    
+        data = self.redis.get(f"token:{token}")
+        if data:
+            return json.loads(data)
+        return None
+    
+    def delete_login_token(self, token: str):
+        if not self.redis:
+            return
+        
+        self.redis.delete(f"login_token:{token}")
+
+    def save_user_session(self, chat_id, session_data):    
+        if 'created_at' not in session_data:
+            session_data['created_at'] = datetime.now().isoformat()
+        session_data['updated_at'] = datetime.now().isoformat()        
+        data_json = json.dumps(session_data)
+
+        self.redis.setex(f"user:{chat_id}", 2592000, data_json)     
+    
+    def get_user_session(self, chat_id):
+        data = self.redis.get(f"user:{chat_id}")
+        if data:
+            return json.loads(data)
+        return None
+    
+    def delete_user_session(self, chat_id):
+        self.redis.delete(f"user:{chat_id}")
+  
         
 redis_client = RedisClient()
