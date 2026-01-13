231b51df21879227666d5b1e766061facd306890
AreS <romanisch08@gmail.com>
2026-01-13 20:49:35 +0300

    Обработка ошибок

--- web_client/src/components/errors/ErrorPage.js
+++ web_client/src/components/errors/ErrorPage.js
@@ -11,10 +11,12 @@ const ErrorPage = () => {
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
             <h1>Ошибка</h1>
-            <p>Не удалось загрузить данные пользователя.</p>
+            <p>Не удалось загрузить данные пользователя. Пожалуйста, попробуйте позже.</p>
             <button onClick={handleGoHome} style={{ padding: '10px 20px', fontSize: '16px' }}>
                 Вернуться на главную
             </button>
         </div>
     );
 };
+
+export default ErrorPage;
